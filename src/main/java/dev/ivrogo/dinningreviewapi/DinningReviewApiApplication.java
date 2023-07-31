a1035aa22bca55965417d4ae928c3735a4e30318
Iván Rodríguez Gómez <er_jotx@hotmail.com>
2023-07-31 19:25:26 +0200

    dependencies +  first steps

---
+++ src/main/java/dev/ivrogo/dinningreviewapi/DinningReviewApiApplication.java
@@ -0,0 +1,13 @@
+package dev.ivrogo.dinningreviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinningReviewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinningReviewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ivrogo/dinningreviewapi/DinningReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ivrogo.dinningreviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinningReviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
